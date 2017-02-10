0cdf83fa4e7b7a1881d36bcd617388c4bc5423ea
Jiyong Lee <xgenpanda@users.noreply.github.com>
2017-02-09 22:33:45 -0500

    update comment
    nothing special

--- bokeh_tutorial.py
+++ bokeh_tutorial.py
@@ -1,5 +1,5 @@
 from bokeh.plotting import figure, output_file, show
-
+#######################
 # prepare some data
 x = [1, 2, 3, 4, 5]
 y = [6, 7, 2, 4, 5]
@@ -15,7 +15,7 @@ p.line(x, y, legend="Temp.", line_width=2)
 
 # show the results
 show(p)
-
+#########################################################
 #########################################################
 
 from bokeh.plotting import figure, output_file, show
